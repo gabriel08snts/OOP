027470286e7a113b80ed2f9fa7723a8de81d4670
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-10-18 17:12:54 -0300

    Add files via upload

---
+++ Estrutural-Poo.js
@@ -0,0 +1,14 @@
+//ESTRUTURAIS;
+let valorHora = 50
+let tempoEstimado = 20
+let desconto = valorHora * tempoEstimado * (10 / 100)
+let custoEstimado = valorHora * tempoEstimado - desconto
+console.log(custoEstimado)
+
+//POO;
+const job = new Job()
+job.valorHora = 50
+job.tempoEstimado = 20
+job.desconto = 10
+const custonEstimado = job.calcularEstimativa()
+console.log(custonEstimado)
